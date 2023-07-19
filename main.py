29182ce6484476d9133202e5a6a20ce148e50da0
aasif <mdaasifxii@gmail.com>
2023-07-19 08:09:23 +0530

    Main file modified

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QTextEdit, QLineEdit, QPushButton
+import sys
+
 
 class ChatBotWindow(QMainWindow):
     def __init__(self):
@@ -7,5 +9,23 @@ class ChatBotWindow(QMainWindow):
 
         self.setMinimumSize(700, 500)
         
-class ChatBot:
-    pass
+        # Add chat area widget
+        self.chat_area = QTextEdit(self)
+        self.chat_area.setGeometry(10,10,480,320)
+        self.chat_area.setReadOnly(True)
+
+        # Add the input field widget
+        self.input_field = QLineEdit(self)
+        self.input_field.setGeometry(10,340,480,40)
+
+
+        # Add the button
+        self.button = QPushButton("Send", self)
+        self.button.setGeometry(500,340,100,40)
+        self.button.setStyleSheet('QPushButton {background-color: #A3C1DA}')
+        self.show()
+
+app = QApplication(sys.argv)
+main_window = ChatBotWindow()
+main_window.setStyleSheet("QMainWindow {background: 'grey';}")
+sys.exit(app.exec())
