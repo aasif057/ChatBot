bebdb4d984255853d85457e2d48803112c6b6024
aasif <mdaasifxii@gmail.com>
2023-07-19 07:51:05 +0530

    Main file added

---
+++ main.py
@@ -0,0 +1,11 @@
+from PyQt6.QtWidgets import QMainWindow
+
+class ChatBotWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+
+        self.setMinimumSize(700, 500)
+        
+class ChatBot:
+    pass
